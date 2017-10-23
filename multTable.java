8cb6e3125cbd246ad22e31614ec96198dcf3d610
josephticode <30960912+josephticode@users.noreply.github.com>
2017-10-22 21:27:34 -0700

    Add files via upload

---
+++ multTable.java
@@ -0,0 +1,16 @@
+
+public class multTable {
+
+	public static void main(String[] args) {
+		int i =0;
+		int f = 0;
+		for (i=1; i <= 10; i++){
+			for (f = 1; f <= 10; f++){
+				System.out.print(i*f + " ");
+			
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
